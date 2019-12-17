152a89e65c934bc89f60606268ff4c0951271df6
Regis A. Despres <regis.despres@publicis.sapient.com>
2019-12-18 00:07:00 +0100

    Update brainio.rb

--- Casks/brainio.rb
+++ Casks/brainio.rb
@@ -2,7 +2,7 @@ cask 'brainio' do
     version '0.0.1-beta.27'
     sha256 '557fa2367d27d295d8c6d6941e07de38bff72804edf154c55b1bf42aa36a1856'
     
-    url "https://cdn.brainio.com/Brainio-{{version}}.dmg"
+    url "https://cdn.brainio.com/Brainio-#{version}.dmg"
     name 'Brainio'
     homepage 'https://brainio.com/'
   

d1304c4c4ec9c68d68580a63b35a0a9adf1b7941
Regis A. Despres <regis.despres@publicis.sapient.com>
2019-12-18 13:03:02 +0100

    Update gear360.rb

--- Casks/gear360.rb
+++ Casks/gear360.rb
@@ -1,6 +1,6 @@
 cask 'gear360' do
     version '180817'
-    sha256 '0c7301b54cfddaf3cf57afa57fa94f163795af347c0b080e1a999a454ea63874'
+    sha256 '052bab2526667411e706207c3ce3e0d702c60fb00c2ba1822a64ef4eed96816a'
     
     url "http://downloadcenter.samsung.com/content/SW/201809/20180917135001161/Gear360Mac_#{version}.pkg"
     name 'Gear360'

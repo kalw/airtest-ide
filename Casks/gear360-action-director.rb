c526148718d6d637da0b42541b3b1ea78555375c
Regis A. Despres <regis.despres@publicis.sapient.com>
2019-12-18 13:03:26 +0100

    Update gear360-action-director.rb

--- Casks/gear360-action-director.rb
+++ Casks/gear360-action-director.rb
@@ -1,6 +1,6 @@
 cask 'gear360-action-director' do
     version '0427'
-    sha256 '0c7301b54cfddaf3cf57afa57fa94f163795af347c0b080e1a999a454ea63874'
+    sha256 'cc40405c8eecb4c3de768894331967d165c7b3855ce227489541849c59dab6ac'
     
     url "http://downloadcenter.samsung.com/content/SW/201704/20170428223105773/Gear360Mac_#{version}_signed.pkg"
     name 'Gear360-Action-Director'

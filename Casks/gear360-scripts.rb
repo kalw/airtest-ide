bc0c3209e94a59b82effdb597d9021fcf30fb613
Regis A. Despres <regis.despres@gmail.com>
2019-12-19 00:42:12 +0100

    Update gear360-scripts.rb

--- Casks/gear360-scripts.rb
+++ Casks/gear360-scripts.rb
@@ -1,7 +1,9 @@
 cask 'gear360-scripts' do
         
-    url "https://github.com/ultramango/gear360pano/archive/master.zip"
+    version "master"
+    url "https://github.com/ultramango/gear360pano/archive/#{version}.zip"
     sha256 :no_check
+    
     name 'Gear360 Scripts'
     homepage "https://github.com/ultramango/gear360pano"
     
@@ -10,6 +12,9 @@ cask 'gear360-scripts' do
     depends_on formula: "multiblend"
     depends_on formula: "parallel"
     depends_on cask: "hugin"
+        
+    binary "gear360pano-master/gear360pano.sh", target: "gear360-jpg2pto"
+    binary "gear360pano-master/gear360video.sh", target: "gear360-mp42pto"
   
     
     

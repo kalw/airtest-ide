3f97c8013637d3351e9060814dee748f92840f27
Regis A. Despres <regis.despres@gmail.com>
2019-12-19 01:08:51 +0100

    Update gear360-scripts.rb

--- Casks/gear360-scripts.rb
+++ Casks/gear360-scripts.rb
@@ -8,11 +8,22 @@ cask 'gear360-scripts' do
     homepage "https://github.com/ultramango/gear360pano"
     
     depends_on formula: "bash"
+    depends_on formula: "coreutils"
     depends_on formula: "ffmpeg"
     depends_on formula: "multiblend"
     depends_on formula: "parallel"
     depends_on cask: "hugin"
-        
+    
+    preflight do
+            system_command '/usr/bin/sed',
+            args: [
+                      '-E',
+                      '-i', '.gear360pano-master/gear360pano.sh.orig',
+                      '-e', 's/readlink /greadlink /g',
+                      'gear360pano-master/gear360pano.sh'
+                  ]
+    end
+    
     binary "gear360pano-master/gear360pano.sh", target: "gear360-jpg2pto"
     binary "gear360pano-master/gear360video.sh", target: "gear360-mp42pto"
   

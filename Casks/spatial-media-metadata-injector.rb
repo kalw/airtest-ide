88a3e894320607861b0783265c8c6a60eca33cfe
Regis A. Despres <regis.despres@gmail.com>
2019-12-20 20:02:04 +0100

    Update spatial-media-metadata-injector.rb

--- Casks/spatial-media-metadata-injector.rb
+++ Casks/spatial-media-metadata-injector.rb
@@ -2,7 +2,7 @@ cask 'spatial-media-metadata-injector' do
 
     version "v2.1"
     url "https://github.com/google/spatial-media/releases/download/#{version}/360.Video.Metadata.Tool.mac.zip"
-    sha256 '0c7301b54cfddaf3cf57afa57fa94f163795af347c0b080e1a999a454ea63874'
+    sha256 'c8de85acfaa247b03d419244f10c42e00c71265956d0762528778f27009abbd9'
     name 'Spatial Media Metadata Injector'
     homepage "https://github.com/google/spatial-media"
 

e868af0e5fbc4adf204641aca7f8cae88bebeed6
Regis A. Despres <regis.despres@gmail.com>
2020-01-16 20:33:22 +0100

    Update snx.rb

--- Formula/snx.rb
+++ Formula/snx.rb
@@ -3,7 +3,7 @@ class Snx < Formula
   homepage "https://checkpoint.com/"
   version "master"
   url "https://remote.fr.publicisgroupe.net/CSHELL/snx_install_osx.sh"
-  sha256 "75f7013a538aa70493e9aa105a846a596f9425df0d2305bde65be6a98dfcb267"
+  sha256 "539574a0594590f280a6858e436072b8701188445973270c974d76c92e319369"
 
   bottle :unneeded
 

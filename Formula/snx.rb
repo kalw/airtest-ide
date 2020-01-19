4757c1c5ca2ace5bb8db89dc03e99e1ff8b264d3
Regis A. Despres <regis.despres@gmail.com>
2020-01-19 13:31:18 +0100

    Update snx.rb

--- Formula/snx.rb
+++ Formula/snx.rb
@@ -6,6 +6,15 @@ class Snx < Formula
   sha256 "539574a0594590f280a6858e436072b8701188445973270c974d76c92e319369"
 
   bottle :unneeded
+  
+  patch p0: "diff --git a/extract.sh b/extract.sh
+index e69de29..cb6a05f 100644
+--- a/extract.sh
++++ b/extract.sh
+@@ -0,0 +1,2 @@
++tail -n +64 #{prefix}/snx_install_osx.sh | bunzip2 -c - > #{prefix}/brew.installer.sh
++
+"
 
   def install
     prefix.install "snx_install_osx.sh"
@@ -22,3 +31,4 @@ class Snx < Formula
     system "script", "-q", "/dev/null", "/usr/local/bin/snx"
   end
 end
+

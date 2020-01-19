fe731ada22e646f9e549ea7244f52b57096e5f0d
Regis A. Despres <regis.despres@gmail.com>
2020-01-19 15:17:29 +0100

    Update snx.rb

--- Formula/snx.rb
+++ Formula/snx.rb
@@ -11,9 +11,8 @@ class Snx < Formula
   def install
     extract_file = "extract.sh"
     prefix.install "snx_install_osx.sh"
-    system "touch", "#{prefix}/extract.sh"
-    (prefix/extract_file).write <<~'EOS'
-    tail -n +64 _PREFIX_/snx_install_osx.sh | bunzip2 -c - > _PREFIX_/brew.installer.sh
+    (prefix/extract_file).write <<~EOS
+      tail -n +64 #{prefix}/snx_install_osx.sh | bunzip2 -c - > #{prefix}/brew.installer.sh
     EOS
     inreplace "#{prefix}/extract.sh", "_PREFIX_", "#{prefix}"
     system "bash", "-x", "extract.sh"

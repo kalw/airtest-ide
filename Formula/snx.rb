bac09b84f8eab8d0c5a82053d292b65e8fe761d7
Regis A. Despres <regis.despres@gmail.com>
2020-01-19 14:47:19 +0100

    Update snx.rb

--- Formula/snx.rb
+++ Formula/snx.rb
@@ -6,12 +6,15 @@ class Snx < Formula
   sha256 "539574a0594590f280a6858e436072b8701188445973270c974d76c92e319369"
 
   bottle :unneeded
-
+  
+  
   def install
     prefix.install "snx_install_osx.sh"
-    system "touch", "#{prefix}/brew.installer.sh"
-    inreplace /^$/, "tail -n +64 #{prefix}/snx_install_osx.sh | bunzip2 -c - > #{prefix}/brew.installer.sh"
-    inreplace "#{prefix}/extract.sh", "_PREEFIX_", "#{prefix}"
+    system "touch", "#{prefix}/extract.sh"
+    ("#{prefix}/extract.sh").write <<~'EOS'
+    tail -n +64 _PREFIX_/snx_install_osx.sh | bunzip2 -c - > _PREFIX_/brew.installer.s
+    EOS
+    inreplace "#{prefix}/extract.sh", "_PREFIX_", "#{prefix}"
     system "bash", "-x", "extract.sh"
     inreplace "#{prefix}/brew.installer.sh", /INSTALL_DIR=.*/, "INSTALL_DIR=#{prefix}"
     inreplace "#{prefix}/brew.installer.sh", "/etc/snx", "#{prefix}/etc/snx"

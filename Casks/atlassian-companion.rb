1b5c3ca573ee0b2e9b84c289a6b6ee9a1f3d609a
Regis A. Despres <regis.despres@gmail.com>
2020-03-05 09:49:48 +0100

    Update atlassian-companion.rb

--- Casks/atlassian-companion.rb
+++ Casks/atlassian-companion.rb
@@ -3,7 +3,7 @@ cask 'atlassian-companion' do
     sha256 '105619763eb622c05d8a4c925f636534d786316040d4eb5490afb37d3484e294'
     
     url "https://update-nucleus.atlassian.com/Atlassian-Companion/291cb34fe2296e5fb82b83a04704c9b4/#{version}/darwin/x64/Atlassian%20Companion.dmg"
-    name 'Brainio'
+    name 'Atlassian Companion'
     homepage 'https://confluence.atlassian.com/doc/atlassian-companion-app-release-notes-958455712.html'
   
     app 'Atlassian Companion.app'

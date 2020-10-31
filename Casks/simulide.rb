c9f0f6fb5754feabf264744566f360c8a7b8e69a
Regis A. Despres <regis.despres@gmail.com>
2020-11-01 00:39:12 +0100

    Create simulide.rb

---
+++ Casks/simulide.rb
@@ -0,0 +1,11 @@
+cask 'simul-ide' do
+
+    version "0.4.13"
+    url "https://mailfence.com/pub/docs/santigoro/web/SimulIDE_#{version}/SimulIDE_#{version}-SR4_Mac.zip"
+    sha256 'fc5633d68105232bbe9f1abc39622801123bb28dd1ed76a1ebc01b35b3d427cd'
+    name 'Simul IDE'
+    homepage "https://www.simulide.com/"
+
+    app "SimulIDE.app"
+
+  end

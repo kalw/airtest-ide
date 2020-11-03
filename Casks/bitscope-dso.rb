5ca6b9e56b91f5d044aa99f127a1ed638fec9344
Regis A. Despres <regis.despres@gmail.com>
2020-11-04 00:11:43 +0100

    Create bitscope-dso.rb

---
+++ Casks/bitscope-dso.rb
@@ -0,0 +1,11 @@
+cask 'bitscope-dso' do
+
+    version "3.0.X37HPRRV"
+    url "https://support.bitscope.com/X37HPRRV/bitscope-dso_#{version}.app.tgz"
+    sha256 '1933bb1c32af2c08d2d1990c8899e7fddacd8103669564f9937cebbaf19b5042'
+    name 'BitScope DSO'
+    homepage "https://support.bitscope.com/X37HPRRV/#downloads"
+
+    app "bitscope-dso.app"
+
+  end

01fa645af7c2a7b069ee93213f7a4f1ee0a4c604
Regis A. Despres <regis.despres@gmail.com>
2020-11-19 00:39:06 +0100

    Update ndi-tools.rb

--- Casks/ndi-tools.rb
+++ Casks/ndi-tools.rb
@@ -2,7 +2,7 @@ cask 'ndi-tools' do
     version '4.5.1'
     sha256 '052bab2526667411e706207c3ce3e0d702c60fb00c2ba1822a64ef4eed96816a'
     
-    url "https://ndi.palakis.fr/runtime/ndi-tools-#{version}-macOS.pkg"
+    url "https://ndi.palakis.fr/tools/ndi-tools-#{version}-macOS.pkg"
     name 'NDI-Tools'
     homepage 'https://ndi.palakis.fr/tools/'
   

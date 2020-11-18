750cc224a649ba07f7e2c39430f783591c69baac
Regis A. Despres <regis.despres@gmail.com>
2020-11-19 00:20:26 +0100

    Create ndi-sdk.rb

---
+++ Casks/ndi-sdk.rb
@@ -0,0 +1,11 @@
+cask 'gear360' do
+    version '4.5.1'
+    sha256 '052bab2526667411e706207c3ce3e0d702c60fb00c2ba1822a64ef4eed96816a'
+    
+    url "https://ndi.palakis.fr/runtime/ndi-runtime-#{version}-macOS.pkg"
+    name 'NDI-SDK'
+    homepage 'https://ndi.palakis.fr/runtime/'
+  
+    pkg "ndi-runtime-#{version}-macOS.pkg", allow_untrusted: true
+    
+  end

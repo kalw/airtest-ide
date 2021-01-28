050c1d9bd136d1502fe98019539caa4d8f568868
Regis A. Despres <regis.despres@gmail.com>
2021-01-28 21:32:07 +0100

    fix: kde-connect version

--- Casks/kde-connect.rb
+++ Casks/kde-connect.rb
@@ -1,6 +1,6 @@
 cask 'kde-connect' do
-    version '1058'
-    sha256 '10f386f6304c8a265a0e1edb7f9452e7a144f78d24cfc1391dac37d57e0e986c'
+    version '1.4-476'
+    sha256 'd473165ba4be7837b9040b5f40b4305bc3cf079742113e0ac45d7c78bb3b791e'
     
     url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
     name 'KDE Connect'

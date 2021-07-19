5b0e7ded2ef2447efa5f4330ee2dfc67e5a301fd
Regis A. Despres <regis.despres@gmail.com>
2021-07-19 16:24:42 +0200

    fix: missing concierge version

--- Formula/concierge.rb
+++ Formula/concierge.rb
@@ -1,7 +1,7 @@
 class Concierge < Formula
   desc "lambdatest hypertest command line utility"
   homepage "https://github.com/fgrehm/vagrant-notify/"
-  #version "v0.6.0"
+  version "v0.0.1"
   url "https://downloads.lambdatest.com/concierge/darwin/concierge"
   sha256 "4358831b7a5af09d80e7f3588ac7ad75c918ba66dcfd9cd463477cd44d78c23a"
 

482e390224a4c36f6f0e466f8ef87437378bee68
Regis A. Despres <regis.despres@gmail.com>
2021-10-19 11:37:02 +0200

    Update kde-connect.rb

--- Casks/kde-connect.rb
+++ Casks/kde-connect.rb
@@ -1,6 +1,6 @@
 cask 'kde-connect' do
     version '21.08.2-741'
-    sha256 'd473165ba4be7837b9040b5f40b4305bc3cf079742113e0ac45d7c78bb3b791e'
+    sha256 '32a66878050f8e4f3eadcaa462ca2f40cf677b3a9c555d90d9fcdb4b17a73d6b'
     
     url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
     name 'KDE Connect'

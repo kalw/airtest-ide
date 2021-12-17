f4cd5be67beea1cf280bc44479e73a44736d447d
Regis A. Despres <regis.despres@gmail.com>
2021-12-17 21:56:03 +0100

    fix: typo

--- Casks/hyperion.rb
+++ Casks/hyperion.rb
@@ -2,7 +2,7 @@ cask 'hyperion' do
     version '2.0.12'
     sha256 'd16c8e2d6e388cf0f65706e4fff5ef93fa829ba74daae79c5bfe33b9af13aacf'
     
-    url "https://github.com/hyperion-project/hyperion.ng/releases/download/${version}/Hyperion-${version}-macOS-x86_64.dmg"
+    url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-x86_64.dmg"
     name 'Hyperion Desktop app'
     homepage 'https://github.com/hyperion-project/hyperion.ng'
   

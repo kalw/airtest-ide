ba0244224604b989424fd7d67a8641e4f98b8430
Regis A. Despres <regis.despres@gmail.com>
2021-12-17 21:56:57 +0100

    fix: hyperion sha

--- Casks/hyperion.rb
+++ Casks/hyperion.rb
@@ -1,6 +1,6 @@
 cask 'hyperion' do
     version '2.0.12'
-    sha256 'd16c8e2d6e388cf0f65706e4fff5ef93fa829ba74daae79c5bfe33b9af13aacf'
+    sha256 '2a84dd8d0815a9ed0a7804536e517352ed9fc4632e2cc4c9aeb059636515c6c6'
     
     url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-x86_64.dmg"
     name 'Hyperion Desktop app'

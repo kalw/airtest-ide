a8ac397aecce72569dd9fe84b20756d0de469f67
Regis A. Despres <regis.despres@gmail.com>
2022-06-28 21:20:12 +0200

    chore(bump): update kde-connect

--- Casks/kde-connect.rb
+++ Casks/kde-connect.rb
@@ -1,6 +1,6 @@
 cask 'kde-connect' do
-    version '21.08.2-741'
-    sha256 '32a66878050f8e4f3eadcaa462ca2f40cf677b3a9c555d90d9fcdb4b17a73d6b'
+    version '22.04.2-988'
+    sha256 '8f9cd0bec10a481d7686a35e9c340f1de78bea93472c8c062c2bed02341212cd'
     
     url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
     name 'KDE Connect'

ecadde32671df1975bf4375f40c7a4fde82eeb5f
Regis A. Despres <regis.despres@gmail.com>
2023-06-25 21:20:51 +0200

    fix: simul-ide.rb sha

--- Casks/simul-ide.rb
+++ Casks/simul-ide.rb
@@ -2,7 +2,7 @@ cask 'simul-ide' do
 
     version "1.0.0"
     url "https://mailfence.com/pub/docs/santigoro/web/SimulIDE_#{version}/SimulIDE_#{version}-SR0_MacOs.zip"
-    sha256 'fc5633d68105232bbe9f1abc39622801123bb28dd1ed76a1ebc01b35b3d427cd'
+    sha256 'e06c7bc2c6e08950eee1245e3b7544d3867deda54916b9735315ee52c336dbf8'
     name 'Simul IDE'
     homepage "https://www.simulide.com/"
 

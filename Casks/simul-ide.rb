d6d44ae820ce0195cc6e999b6013d279e8e77e14
Regis A. Despres <regis.despres@gmail.com>
2023-06-25 21:19:31 +0200

    chore: bump simul-ide.rb to 1.0.0

--- Casks/simul-ide.rb
+++ Casks/simul-ide.rb
@@ -1,7 +1,7 @@
 cask 'simul-ide' do
 
-    version "0.4.13"
-    url "https://mailfence.com/pub/docs/santigoro/web/SimulIDE_#{version}/SimulIDE_#{version}-SR4_Mac.zip"
+    version "1.0.0"
+    url "https://mailfence.com/pub/docs/santigoro/web/SimulIDE_#{version}/SimulIDE_#{version}-SR0_MacOs.zip"
     sha256 'fc5633d68105232bbe9f1abc39622801123bb28dd1ed76a1ebc01b35b3d427cd'
     name 'Simul IDE'
     homepage "https://www.simulide.com/"
